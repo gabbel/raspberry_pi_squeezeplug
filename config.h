24a8982191566044557a6eb8d0f2cd51d469162f
Gabbel <gwinkler@7wink.ch>
2014-05-10 14:09:31 +0200

    Refactored Code. Added Flowcharts.

--- config.h
+++ config.h
@@ -2,6 +2,7 @@
 //#define DEBUG          //uncomment to disable debugging output
 
 //-----------------defines------------------------------
+// Timings
 #define WAIT_CYCLE  1   //in miliseconds (must be a common divisor of the following 3 defines)
 #define ENC_READ_TIME 1 //in miliseconds
 #define ENC_SEND_TIME 20 //in miliseconds
@@ -9,15 +10,16 @@
  
 #define LONG_PRESS_TIME 500 //in miliseconds
 
-
+// Display
 #define DISPLAY  ":0"   //NULL = user environment variable DISPLAY. Otherwise pass a string for the display host:displaynumber e.g. :0
 
+// Rotary Encoder
+#define ROTARY_ENC_PIN_A    RPI_V2_GPIO_P1_18 // Pin A
+#define ROTARY_ENC_PIN_B    RPI_V2_GPIO_P1_16 // Pin B
 
-#define ROTARY_ENC_PIN_A    RPI_V2_GPIO_P1_16
-#define ROTARY_ENC_PIN_B    RPI_V2_GPIO_P1_18
-
-#define ROTARY_ENC_KEYCODE_LEFT XK_Up
-#define ROTARY_ENC_KEYCODE_RIGHT XK_Down
+// Keys, Rotary Encoder Keyboard events
+#define ROTARY_ENC_KEYCODE_LEFT XK_Up // Scroll Up (Rotary Encoder)
+#define ROTARY_ENC_KEYCODE_RIGHT XK_Down // Scroll Down (Rotary Encoder)
 
 
 //----------------calculations------------------------
@@ -26,17 +28,18 @@
 #define LONG_PRESS_CYCLES ((LONG_PRESS_TIME)/(WAIT_CYCLE)/(KEY_READ_CYCLES)) //how many times do we need to read the keys to define a longpress
 #define ENC_READ_CYCLES ((ENC_READ_TIME)/(WAIT_CYCLE)) //how many cycles to wait to read the encoder
 #define ENC_SEND_CYCLES ((ENC_SEND_TIME)/(WAIT_CYCLE)) //how many cycles to wait to send the encoder state
+#define NUM_PINS (sizeof(pin_configuration)/ sizeof(pin_setting_t)) //how many pins are configured
 
 //----------------structures --------------------------
-#include <stdbool.h>
-#include <stdint.h>
-#include <bcm2835.h>
-#include <X11/keysym.h>
+#include <stdbool.h> // for bool datatype
+#include <stdint.h>  //for uint_*t and int_*t datatypes
+#include <bcm2835.h> // Library for of the BCM2835 SOC
+#include <X11/keysym.h> // Headerfile for the X11 keycode defines
 
 typedef struct pin_setting_s {
 	uint8_t pin;   //the pin to press. one of RPI_V2_GPIO_P1_XX
-	unsigned int keycode_short; //the keycode to send
-	unsigned int keycode_long;
+	unsigned int keycode_short; //the keycode to send for a short press event
+	unsigned int keycode_long; //the keycode to send for a long press event
 
 } pin_setting_t;
 
@@ -44,37 +47,37 @@ typedef struct pin_setting_s {
 
 
 //-------- Assign Keyboard events to pins ---------------------
+//see \usr\include\X11\keysymdef.h for the keycode defines
 const pin_setting_t pin_configuration[] = {
 
-                                                                                                                                //PIN   Command Short                                   Command Long
-                                                                                                                                //01    Pin #1 = +3.3V (Primary
-                                                                                                                                //02    Pin #2 = +5V (Primary)
-//      { RPI_V2_GPIO_P1_03, 0 , 0 },                                                           //03    Not in use                                              Not in use
-                                                                                                                                //04    Pin #4 = +5V
-//      { RPI_V2_GPIO_P1_05, 0 , 0 },                                                           //05    Not in use                                              Not in use
-                                                                                                                                //06    Pin #6 = Ground
-        { RPI_V2_GPIO_P1_07, XK_minus , XK_minus },                                     //07    Vol -                                                   Same as short
-//      { RPI_V2_GPIO_P1_08, 0 , 0 },                                                           //08    UART
-                                                                                                                                //09    Pin #9 = Ground
-//      { RPI_V2_GPIO_P1_10, 0 , 0 },                                                           //10    UART
-        { RPI_V2_GPIO_P1_11, XK_plus, XK_plus },                                        //11    Vol +                                                   Same as short
-        { RPI_V2_GPIO_P1_12, XK_Return, 0 },                                            //12    Enter (Go)                                              Not in use
-//      { RPI_V2_GPIO_P1_13, 0 , 0 },                                                           //13    Not in use                                              Not in use
-                                                                                                                                //14    Pin #14 = Ground
-        { RPI_V2_GPIO_P1_15, XK_c, 0 },                                                         //15    Pause                                                   -
-//      { RPI_V2_GPIO_P1_16, 0 , 0 },                                                           //16    Scroll Down (Rotary Encoder)
-                                                                                                                                //17    Pin #17 = +3.3V
-//      { RPI_V2_GPIO_P1_18, 0 , 0 },                                                           //18    Scroll Up (Rotary Encoder)
-        { RPI_V2_GPIO_P1_19, XK_a, XK_A },                                                      //19    Add                                                             Add (hold)
-                                                                                                                                //20    Pin #20 = Ground
-        { RPI_V2_GPIO_P1_21, XK_p, XK_P },                                                      //21    Play
-        { RPI_V2_GPIO_P1_22, XK_b, XK_B },                                                      //22    Skip Track (Forward)                    Fwd (hold)
-        { RPI_V2_GPIO_P1_23, XK_Escape, XK_h },                                           //23    Back                                                    Home
-        { RPI_V2_GPIO_P1_24, XK_z, XK_Z },                                                      //24    Skip Track (Back)                               Rew (hold)
-                                                                                                                                //25    Pin #25 = Ground
-        { RPI_V2_GPIO_P1_26, XK_bracketleft, XK_bracketright }          //26    Now Playing                                             Show Playlist
+ //PIN Command Short Command Long
+												//01 Pin #1 = +3.3V (Primary
+												//02 Pin #2 = +5V (Primary)
+// { RPI_V2_GPIO_P1_03, 0 , 0 }, 				//03 Not in use Not in use
+												//04 Pin #4 = +5V
+// { RPI_V2_GPIO_P1_05, 0 , 0 }, 				//05 Not in use Not in use
+												//06 Pin #6 = Ground
+ { RPI_V2_GPIO_P1_07, XK_minus , XK_minus }, 	//07 Vol - Same as short
+// { RPI_V2_GPIO_P1_08, 0 , 0 }, 				//08 UART
+												//09 Pin #9 = Ground
+// { RPI_V2_GPIO_P1_10, 0 , 0 }, 				//10 UART
+ { RPI_V2_GPIO_P1_11, XK_plus, XK_plus }, 		//11 Vol + Same as short
+// { RPI_V2_GPIO_P1_12, 0 , 0 }, 				//12 Not in use Not in use
+ { RPI_V2_GPIO_P1_13, XK_c, 0 }, 				//13 Pause -
+												//14 Pin #14 = Ground
+ { RPI_V2_GPIO_P1_15, XK_Return, 0 },			//15 Enter (Go) Not in use
+// { RPI_V2_GPIO_P1_16, 0 , 0 }, 				//16 Scroll Down (Rotary Encoder)
+												//17 Pin #17 = +3.3V
+// { RPI_V2_GPIO_P1_18, 0 , 0 }, 				//18 Scroll Up (Rotary Encoder)
+ { RPI_V2_GPIO_P1_19, XK_a, XK_A }, 			//19 Add Add (hold)
+												//20 Pin #20 = Ground
+ { RPI_V2_GPIO_P1_21, XK_p, XK_P }, 			//21 Play
+ { RPI_V2_GPIO_P1_22, XK_b, XK_B }, 			//22 Skip Track (Forward) Fwd (hold)
+ { RPI_V2_GPIO_P1_23, XK_Escape, XK_h }, 		//23 Back Home
+ { RPI_V2_GPIO_P1_24, XK_z, XK_Z }, 			//24 Skip Track (Back) Rew (hold)
+												//25 Pin #25 = Ground
+ { RPI_V2_GPIO_P1_26, XK_bracketleft, XK_bracketright } //26 Now Playing Show Playlist
 };
 
 
-#define NUM_PINS (sizeof(pin_configuration)/ sizeof(pin_setting_t))
 
--- core.c
+++ core.c
@@ -51,176 +51,193 @@ Used Resources:
 
 
 
-//handler for sigterm event
+//handler for sigterm event which is called when you press Ctrl+C
 void sigterm_handler(int signum) {
-	terminate = true;
+	terminate = true; //set a flag (will be read in main loop)
 }
 
-//Setups the chip, configures the pins  and returns true on sucess
-bool setup() {
-	if (!bcm2835_init()) { //when chip init fails
-        	fprintf(stderr,"Failed to init bcm2835\n");
-		return false;
+//Helper Function, Setups the chip, configures the pins and returns true on success
+bool setup(void) {
+	//-------------------Configure Display-------------------------
+   	display = XOpenDisplay(DISPLAY);
+   	if(display == NULL) {
+		fprintf(stderr,"Failed open display %s.\n",DISPLAY);
+      	return false;
 	}
 
+	//----------------Configure Pins --------------------------
+	if (!bcm2835_init()) { //when chip init fails
+        fprintf(stderr,"Failed to init bcm2835\n");
+		return false;
+	}
 	
 	for(int i=0; i<number_of_pins; i++) { //once for each pin
 		pin_setting_t pin_setting = pin_configuration[i]; //get the current pin
 		pin_state[i] = 1;	
 	
-    		// Set RPI pin to be an input
-    		bcm2835_gpio_fsel(pin_setting.pin, BCM2835_GPIO_FSEL_INPT);
-    		//  with a pullup
-    		bcm2835_gpio_set_pud(pin_setting.pin, BCM2835_GPIO_PUD_UP);
+		// Set RPI pin to be an input
+		bcm2835_gpio_fsel(pin_setting.pin, BCM2835_GPIO_FSEL_INPT);
+		//  with a pullup
+		bcm2835_gpio_set_pud(pin_setting.pin, BCM2835_GPIO_PUD_UP);
 	}
 
-	//configure rotary encoder
+	//------------------Configure rotary encoder-------------------------
 	bcm2835_gpio_fsel(ROTARY_ENC_PIN_A, BCM2835_GPIO_FSEL_INPT);
-    	bcm2835_gpio_set_pud(ROTARY_ENC_PIN_A, BCM2835_GPIO_PUD_UP);
+    bcm2835_gpio_set_pud(ROTARY_ENC_PIN_A, BCM2835_GPIO_PUD_UP);
 	bcm2835_gpio_fsel(ROTARY_ENC_PIN_B, BCM2835_GPIO_FSEL_INPT);
-    	bcm2835_gpio_set_pud(ROTARY_ENC_PIN_B, BCM2835_GPIO_PUD_UP);
+    bcm2835_gpio_set_pud(ROTARY_ENC_PIN_B, BCM2835_GPIO_PUD_UP);
 
  	int8_t new = 0;
-	if(bcm2835_gpio_lev(ROTARY_ENC_PIN_A)==HIGH)
+	if(bcm2835_gpio_lev(ROTARY_ENC_PIN_A)==HIGH) {
 		new = 3;
-	if(bcm2835_gpio_lev(ROTARY_ENC_PIN_B)==HIGH)
-	    	new ^= 1;                   // convert gray to binary
+	}
+	if(bcm2835_gpio_lev(ROTARY_ENC_PIN_B)==HIGH) {
+	    new ^= 1;                   // convert gray to binary
+	}
 	  
 	enc_last = new;                   // power on state
 	enc_delta = 0;	
 
 
-	// Obtain the X11 display.
-   	display = XOpenDisplay(DISPLAY);
-   	if(display == NULL) {
-		fprintf(stderr,"Failed open display\n");
-      		return false;
-	}
-
-
-
-	//register sigterm handler
-	signal(SIGINT, sigterm_handler);
+	//-------------------Configure Sigterm Handler ----------------
+	signal(SIGINT, sigterm_handler);//register sigterm handler
 
 	return true;
 }
 
+//Helper function to free resources before terminating
+void takedown(void) {
+	bcm2835_close();
+	XCloseDisplay(display);
+}
+
+//Helper Function to send a keyevent to x11
 void send_event (unsigned int keycode, bool pressed) {
 	//Code from: https://shanetully.com/2012/07/simulating-mediakey-presses-in-x11/
 	XTestFakeKeyEvent(display, XKeysymToKeycode(display, keycode), pressed, 0);
 	XFlush(display);	
 }
 
- 
+//Helper function to read the encoder value
 int8_t encode_read1(void)         // read single step encoders
 {
-  int8_t val= enc_delta;
-  enc_delta = 0; 
-  return val;                   // counts since last call
+	int8_t val= enc_delta;
+	enc_delta = 0; 
+	return val;                   // counts since last call
 }
 
-void do_cycle(void) {
+//Reads the push buttons and sends the events to x11
+void key_read(void) {
+	for(int i=0; i<number_of_pins; i++) { //once for each pin
+		pin_setting_t pin_setting = pin_configuration[i]; //get the current pin configuration
+		bool current_state = (bcm2835_gpio_lev(pin_setting.pin)==HIGH); //get current state from bcm2835 chip
+		bool old_state = pin_state[i]; //fetch old state
 
-	if(++key_read_time == KEY_READ_CYCLES) { //increment counter and compare
-		key_read_time=0;
-
-		for(int i=0; i<number_of_pins; i++) { //once for each pin
-			pin_setting_t pin_setting = pin_configuration[i]; //get the current pin
-			bool current_state = (bcm2835_gpio_lev(pin_setting.pin)==HIGH);
-			bool old_state = pin_state[i];
-		
-
-			if(!current_state) { //button is LOW
-				if(pin_pressed_time[i] == LONG_PRESS_CYCLES && pin_setting.keycode_long!=0 ) {
-					#ifdef DEBUG
-						printf("send long press down\n");
-					#endif
-					send_event(pin_setting.keycode_long,true);
-				}
-
-				pin_pressed_time[i]++;
+		if(!current_state) { //Button is LOW = pressed
+			if(pin_pressed_time[i] == LONG_PRESS_CYCLES && pin_setting.keycode_long!=0 ) { //button pressed long enough for long press
+				#ifdef DEBUG
+					printf("send long press down, pin %u\n",pin_setting.pin);
+				#endif
+				send_event(pin_setting.keycode_long,true);
 			}
 
-			if(current_state != old_state && current_state) { //if butten released = positive edge detected
-				if(pin_pressed_time[i] > LONG_PRESS_CYCLES) {
-					if(pin_setting.keycode_long!=0) {
+			pin_pressed_time[i]++;
+		} else { //Button is HIGH = not pressed
+			if(!old_state) { //Button released = positive edge detected
+				if(pin_pressed_time[i] > LONG_PRESS_CYCLES) { //it has been a long press
+					if(pin_setting.keycode_long!=0) { //long press functionality was configured for this pin
 					#ifdef DEBUG
-						printf("send long press up\n");
+						printf("send long press up, pin %u\n",pin_setting.pin);
 					#endif
 						send_event(pin_setting.keycode_long,false);
 					}
-				} else {
+				} else { //it has been a short press
 					#ifdef DEBUG
-						printf("send short press down\n");
+						printf("send short press down, pin %u\n",pin_setting.pin);
 					#endif
 					send_event(pin_setting.keycode_short,true);
 					
 					#ifdef DEBUG
-						printf("send short press up\n");
+						printf("send short press up, pin %u\n",pin_setting.pin);
 					#endif
 					send_event(pin_setting.keycode_short,false);
 				}
 				pin_pressed_time[i] = 0;
-			} 		
-			
-			pin_state[i] = current_state;		
+			} 	
+		}		
+		
+		pin_state[i] = current_state; //save current state to old state
+	}
+}
+
+//Reads the Encoder changes
+void enc_read(void) {
+	//http://www.mikrocontroller.net/articles/Drehgeber#Dekoder_f.C3.BCr_Drehgeber_mit_wackeligen_Rastpunkten
+	enc_last = (enc_last << 2)  & 0x0F;
+
+	if(bcm2835_gpio_lev(ROTARY_ENC_PIN_A)==HIGH) {
+		enc_last |= 2;
+	}
+	if(bcm2835_gpio_lev(ROTARY_ENC_PIN_B)==HIGH) {
+		enc_last |=1;
+	}
+	enc_delta += table[enc_last];
+}
+
+//Sends the encoder changes to X11
+void enc_send(void) {
+	int8_t delta = encode_read1();
+	
+	if(delta!=0) {
+		#ifdef DEBUG
+			printf("Rotary Encoder %d\n",delta);
+		#endif
+
+		int8_t amount = abs(delta);
+		unsigned int keycode = ROTARY_ENC_KEYCODE_RIGHT;
+
+		if(delta<0) {
+			keycode=ROTARY_ENC_KEYCODE_LEFT;
+		} 		
+		for(int i=0; i<amount; i++) {
+			send_event(keycode,true); //pressed event
+			send_event(keycode,false); //released event
 		}
+	} 
+}
+
+//Main State Machine, is called in a fixed interval
+void do_cycle(void) {
+	if(++key_read_time == KEY_READ_CYCLES) { //increment counter and compare
+		key_read_time=0;
+		key_read();
 	}
 
 	if(++enc_read_time == ENC_READ_CYCLES) { //increment counter and compare
 		enc_read_time=0;
-		//http://www.mikrocontroller.net/articles/Drehgeber#Dekoder_f.C3.BCr_Drehgeber_mit_wackeligen_Rastpunkten
-		enc_last = (enc_last << 2)  & 0x0F;
-
-		if(bcm2835_gpio_lev(ROTARY_ENC_PIN_A)==HIGH)
-			enc_last |= 2;
-		if(bcm2835_gpio_lev(ROTARY_ENC_PIN_B)==HIGH)
-			enc_last |=1;
-		enc_delta += table[enc_last];
+		enc_read();
 	}
 		
-
 	if(++enc_send_time == ENC_SEND_CYCLES) { //increment counter and compare
 		enc_send_time=0;
-		int8_t delta = encode_read1();
-		
-		if(delta!=0) {
-			#ifdef DEBUG
-				printf("Rotary Encoder %d\n",delta);
-			#endif
-
-			int8_t amount = abs(delta);
-			unsigned int keycode = ROTARY_ENC_KEYCODE_RIGHT;
-
-			if(delta<0) {
-				keycode=ROTARY_ENC_KEYCODE_LEFT;
-			} 		
-			for(int i=0; i<amount; i++) {
-				send_event(keycode,true); //pressed event
-				send_event(keycode,false); //released event
-			}
-		} 
+		enc_send();
 	}
-
 }
 
-
-
-
+//Main routine, is called on programm startup
 int main() {
-	
 	if(!setup()) { //if setup fails
 		fprintf(stderr, "Setup failed\n");
-		return -1;
+		return 1;
 	}
 	fprintf(stdout,"Setup ok\n");
 
-	while(!terminate) {
+	while(!terminate) { //while no SIGTERM received ( Kill signal )
 		bcm2835_delay(WAIT_CYCLE); // sleep for WAIT_CYCLE miliseconds*/
 		do_cycle();
 	}
 	
-	bcm2835_close();
-	XCloseDisplay(display);
+	takedown();
 	fprintf(stdout,"\nTerminated\n");
+	return 0;
 }
