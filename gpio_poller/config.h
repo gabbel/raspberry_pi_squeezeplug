afe77ede18276d58038c5244a49f3724ec3b2f98
gabbel <gabbel@users.noreply.github.com>
2014-05-10 20:53:03 +0200

    Update config.h
    changed comments

--- gpio_poller/config.h
+++ gpio_poller/config.h
@@ -50,33 +50,33 @@ typedef struct pin_setting_s {
 //see \usr\include\X11\keysymdef.h for the keycode defines
 const pin_setting_t pin_configuration[] = {
 
- //PIN Command Short Command Long
-												//01 Pin #1 = +3.3V (Primary
-												//02 Pin #2 = +5V (Primary)
-// { RPI_V2_GPIO_P1_03, 0 , 0 }, 				//03 Not in use Not in use
-												//04 Pin #4 = +5V
-// { RPI_V2_GPIO_P1_05, 0 , 0 }, 				//05 Not in use Not in use
-												//06 Pin #6 = Ground
- { RPI_V2_GPIO_P1_07, XK_minus , XK_minus }, 	//07 Vol - Same as short
-// { RPI_V2_GPIO_P1_08, 0 , 0 }, 				//08 UART
-												//09 Pin #9 = Ground
-// { RPI_V2_GPIO_P1_10, 0 , 0 }, 				//10 UART
- { RPI_V2_GPIO_P1_11, XK_plus, XK_plus }, 		//11 Vol + Same as short
-// { RPI_V2_GPIO_P1_12, 0 , 0 }, 				//12 Not in use Not in use
- { RPI_V2_GPIO_P1_13, XK_c, 0 }, 				//13 Pause -
-												//14 Pin #14 = Ground
- { RPI_V2_GPIO_P1_15, XK_Return, 0 },			//15 Enter (Go) Not in use
-// { RPI_V2_GPIO_P1_16, 0 , 0 }, 				//16 Scroll Down (Rotary Encoder)
-												//17 Pin #17 = +3.3V
-// { RPI_V2_GPIO_P1_18, 0 , 0 }, 				//18 Scroll Up (Rotary Encoder)
- { RPI_V2_GPIO_P1_19, XK_a, XK_A }, 			//19 Add Add (hold)
-												//20 Pin #20 = Ground
- { RPI_V2_GPIO_P1_21, XK_p, XK_P }, 			//21 Play
- { RPI_V2_GPIO_P1_22, XK_b, XK_B }, 			//22 Skip Track (Forward) Fwd (hold)
- { RPI_V2_GPIO_P1_23, XK_Escape, XK_h }, 		//23 Back Home
- { RPI_V2_GPIO_P1_24, XK_z, XK_Z }, 			//24 Skip Track (Back) Rew (hold)
-												//25 Pin #25 = Ground
- { RPI_V2_GPIO_P1_26, XK_bracketleft, XK_bracketright } //26 Now Playing Show Playlist
+							//PIN# Command Short, Command Long
+							//01 Pin #1 = +3.3V (Primary
+							//02 Pin #2 = +5V (Primary)
+// { RPI_V2_GPIO_P1_03, 0 , 0 }, 			//03 Not in use, Not in use
+							//04 Pin #4 = +5V
+// { RPI_V2_GPIO_P1_05, 0 , 0 }, 			//05 Not in use, Not in use
+							//06 Pin #6 = Ground
+ { RPI_V2_GPIO_P1_07, XK_minus , XK_minus }, 		//07 Vol -, Same as short
+// { RPI_V2_GPIO_P1_08, 0 , 0 }, 			//08 UART
+							//09 Pin #9 = Ground
+// { RPI_V2_GPIO_P1_10, 0 , 0 }, 			//10 UART
+ { RPI_V2_GPIO_P1_11, XK_plus, XK_plus }, 		//11 Vol +, Same as short
+// { RPI_V2_GPIO_P1_12, 0 , 0 }, 			//12 Not in use, Not in use
+ { RPI_V2_GPIO_P1_13, XK_c, 0 }, 			//13 Pause, -
+							//14 Pin #14 = Ground
+ { RPI_V2_GPIO_P1_15, XK_Return, 0 },			//15 Enter (Go), Not in use
+// { RPI_V2_GPIO_P1_16, 0 , 0 }, 			//16 Scroll Down (Rotary Encoder)
+							//17 Pin #17 = +3.3V
+// { RPI_V2_GPIO_P1_18, 0 , 0 }, 			//18 Scroll Up (Rotary Encoder)
+ { RPI_V2_GPIO_P1_19, XK_a, XK_A }, 			//19 Add, Add (hold)
+							//20 Pin #20 = Ground
+ { RPI_V2_GPIO_P1_21, XK_p, XK_P }, 			//21 Play, -
+ { RPI_V2_GPIO_P1_22, XK_b, XK_B }, 			//22 Skip Track (Forward), Fwd (hold)
+ { RPI_V2_GPIO_P1_23, XK_Escape, XK_h }, 		//23 Back, Home
+ { RPI_V2_GPIO_P1_24, XK_z, XK_Z }, 			//24 Skip Track (Back), Rew (hold)
+							//25 Pin #25 = Ground
+ { RPI_V2_GPIO_P1_26, XK_bracketleft, XK_bracketright } //26 Now Playing, Show Playlist
 };
 
 
