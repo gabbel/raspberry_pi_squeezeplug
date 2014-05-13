4c15efa60253f0810a1220f19d4b2eccc6ad1a0e
gabbel <gabbel@users.noreply.github.com>
2014-05-13 08:05:47 +0200

    Update autobuild.sh
    Added Version and comment

--- gpio_poller/autobuild.sh
+++ gpio_poller/autobuild.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
+# Version: v0.1 Date: 2014-05-13
+
+# Use gcc to build the "io_poller" binary using core.c the bcm2835 library, X11 library and X11test library. Build with optimized settings.
 gcc -std=c99 -o io_poller core.c -l bcm2835 -lX11 -lXtst -O3
 
 
